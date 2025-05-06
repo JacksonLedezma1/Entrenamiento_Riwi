423c3171cad35e8ee25f8d840d9933f5f14764ea
Jackson Ledezma <jacksonledezma01@gmail.com>
2025-05-06 09:38:56 -0500

    update

--- practica1/semana 1/ejercicio_avanzado_7.py
+++
@@ -1,6 +0,0 @@
-Km=float(input("Ingrese los Km a convertir: "))
-if Km>0: 
-    metros=Km*1000
-    centimetro=Km*100000
-    milimetros=Km*1000000
-    print(f"{Km} Km={metros} Metros, {centimetro} cm, {milimetros} mm")
